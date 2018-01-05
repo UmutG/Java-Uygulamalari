cd6141c4ab329c6e6cd18ab49a20d693dbdc5bdd
Umut Geyik <umut.geyik@hotmail.com>
2018-01-05 18:53:59 +0300

    Add files via upload

---
+++ Örnek04.java
@@ -0,0 +1,21 @@
+package ornekler;
+import java.util.Scanner;
+public class Örnek04 {
+	public static void main(String[] args) {
+		int sayi, yuzler, onlar, birler;
+		Scanner tara = new Scanner(System.in);
+			do {
+				sayi=0;
+				System.out.print("3 basamaklý sayýyý giriniz: ");
+				sayi = tara.nextInt();
+			}
+			while(String.valueOf(sayi).length() != 3);
+			tara.close();
+			yuzler = sayi/100;
+			onlar = (sayi-yuzler*100)/10;
+			birler = (sayi-(yuzler*100)-onlar*10);
+			int carpim = yuzler*onlar*birler, toplam = yuzler+onlar+birler;
+			System.out.print("Kalan = " + (carpim%toplam));
+	}
+
+}
