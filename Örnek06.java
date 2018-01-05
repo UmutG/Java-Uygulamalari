17a7efcccaf8d5d3c5baf48fcd22c62c3b5b29f0
Umut Geyik <umut.geyik@hotmail.com>
2018-01-05 19:12:15 +0300

    Add files via upload

---
+++ Örnek06.java
@@ -0,0 +1,21 @@
+package ornekler;
+public class Örnek06 {
+	public static void main(String[] args) {
+		int top=0, bes=0, yedi=0, ikisi=0;
+		for(int i=1; i<=1000; i++)
+		{
+			if(i%5==0)
+				bes++;
+			if(i%7!=0)
+				yedi++;
+			if(i%5==0 && i%7!=0)
+			{
+				ikisi++;
+				System.out.println(i + "\n" + "----");
+				top+=i;
+			}
+		}
+		System.out.println("Toplam = " + top + ". Beþe tam bölünenler = " + bes + " tane. Yediye tam bölünemeyenler = " + yedi + " tane. \n"
+				+ "Beþe tam bölünüp yediye tam bölünemeyenlerin sayýsý: " + ikisi);
+	}
+}
