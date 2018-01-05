4c5b4fdad1c80c59c0ae533f5e9759cc5634cd09
Umut Geyik <umut.geyik@hotmail.com>
2018-01-05 18:59:33 +0300

    Add files via upload

---
+++ Örnek05.java
@@ -0,0 +1,16 @@
+package ornekler;
+import java.util.Scanner;
+public class Örnek05 {
+	public static void main(String[] args) {
+		int i;
+		String soyle;
+		Scanner tara = new Scanner(System.in);
+		System.out.print("Neler söylemek istersiniz? : ");
+		soyle = tara.nextLine();
+		System.out.print("Kaç defa tekrarlansýn? : ");
+		i = tara.nextInt();
+		tara.close();
+		for (int j=0; j<i; j++)
+			System.out.println(soyle);
+	}
+}
