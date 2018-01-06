672e3a6b76c9c8178abb518219cfad8c95461c3d
Umut Geyik <umut.geyik@hotmail.com>
2018-01-07 01:40:47 +0300

    Update Örnek04.java

--- Örnek04.java
+++ Örnek04.java
@@ -2,22 +2,16 @@ package ornekler;
 import java.util.Scanner;
 public class Örnek04 {
 	public static void main(String[] args) {
-		//Klavyeden girilen üç basamaklı sayının rakamları çarpımının, rakamları toplamına bölümünden kalanı bulan program
-		//Negatif hesaplamasını daha sonra ekleyeceğim
-		int sayi, yuzler, onlar, birler;
+		//Klavyeden girilen üç basamaklı sayının rakamları çarpımının, rakamları toplamına bölümünden kalanı bulan program		int sayi, yuzler, onlar, birler;
 		Scanner tara = new Scanner(System.in);
-			do {
-				sayi=0;
-				System.out.print("3 basamaklı sayıyı giriniz: ");
-				sayi = tara.nextInt();
-			}
-			while(String.valueOf(sayi).length() != 3);
-			tara.close();
-			yuzler = sayi/100;
-			onlar = (sayi-yuzler*100)/10;
-			birler = (sayi-(yuzler*100)-onlar*10);
-			int carpim = yuzler*onlar*birler, toplam = yuzler+onlar+birler;
-			System.out.print("Kalan = " + (carpim%toplam));
+		System.out.print("3 basamaklı sayıyı giriniz: ");
+		sayi = tara.nextInt();
+		tara.close();
+		yuzler = sayi/100;
+		onlar = (sayi-yuzler*100)/10;
+		birler = (sayi-(yuzler*100)-onlar*10);
+		int carpim = yuzler*onlar*birler, toplam = yuzler+onlar+birler;
+		System.out.print("Kalan = " + (carpim%toplam));
 	}
 
 }
