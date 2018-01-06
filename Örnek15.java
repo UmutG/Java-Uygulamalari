afc159350fe80d1170a739376aeb43ade6ba0837
Umut Geyik <umut.geyik@hotmail.com>
2018-01-06 16:43:05 +0300

    Add files via upload

---
+++ Örnek15.java
@@ -0,0 +1,13 @@
+package ornekler;
+import java.util.Scanner;
+public class Örnek15 {
+	public static void main(String[] args) {
+		//Klavyeden girilen metni terse çeviren program
+		System.out.print("Haydi yaz: ");
+		Scanner tara = new Scanner(System.in);
+		String text = tara.nextLine();
+		tara.close();
+		System.out.println(new StringBuilder(text).reverse());
+	}
+
+}
