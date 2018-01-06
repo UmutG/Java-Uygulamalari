f6fa168b10d94626eca98ab61e2a99f95289dddc
Umut Geyik <umut.geyik@hotmail.com>
2018-01-06 23:25:26 +0300

    Add files via upload

---
+++ Örnek30.java
@@ -0,0 +1,24 @@
+package ornekler;
+import java.util.Scanner;
+public class Örnek30 {
+	public static int bak(String str) {
+		int say=0;
+		for(int i=0; i<str.length(); i++) {
+			if(((char)str.charAt(i)) >= 'A' && (char)str.charAt(i) <= 'Z'
+					|| (char)str.charAt(i) >= 'a' && (char)str.charAt(i) <= 'z')
+				say++;
+			}
+		return say;
+	}
+	public static void main(String[] args) {
+		//Klavyeden girilen bir kelimenin kaç harf olduğunu gösteren program
+		//UTF-8 desteği olmadığı için Türkçe harflere duyarsız.
+		Scanner tara = new Scanner(System.in);
+		System.out.print("Mikrofon sende: ");
+		String a = tara.nextLine();
+		tara.close();
+		System.out.println("Cümlede " + bak(a) + " harf var.");
+		
+	}
+
+}
