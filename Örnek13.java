bf4e48e6f98886786d073ac82c1684eaeb6b163a
Umut Geyik <umut.geyik@hotmail.com>
2018-01-06 23:44:53 +0300

    Update Örnek13.java

--- Örnek13.java
+++ Örnek13.java
@@ -7,7 +7,7 @@ public class Örnek13 {
         for(int i=2; i<=1000; i++)
         {
             sayi=0;
-            for(int j=2; j<=i/2; j++)
+            for(int j=2; j<i; j++)
             {
                 if(i%j==0) 
                 	sayi++;
