39a6d37860ca9ac9646c8268f1062589a558d1cc
Umut Geyik <umut.geyik@hotmail.com>
2018-01-06 20:13:08 +0300

    Add files via upload

---
+++ Örnek22.java
@@ -0,0 +1,27 @@
+package ornekler;
+import java.util.Random;
+import java.util.Scanner;
+public class Örnek22 {
+	public static void main(String[] args)
+	//1'den 100'e kadar 10 haklı sayı tahmin oyunu (kullanıcı bilgilendirilir)
+	{
+		Random a = new Random();
+		int rast = a.nextInt(100);
+		Scanner tara = new Scanner(System.in);
+		for (int i=10; i>=0; i--)
+		{
+			System.out.print("Tahmininizi giriniz: ");
+			int tahmin = tara.nextInt();
+			if(tahmin == rast)
+			{
+				System.out.print("Tahmininiz doğru. Tutulan sayı = " + rast);
+				break;
+			}
+			else if(tahmin>rast)
+				System.out.println("Tahminin sayıdan büyük. Sayıyı düşür.");
+			else if(tahmin<rast)
+				System.out.println("Tahminin sayıdan küçük. Sayıyı yükselt.");
+		}
+		tara.close();
+	}
+}
