2144f00573edca4dcc70e77ceae05debd65ddbbd
Umut Geyik <umut.geyik@hotmail.com>
2018-01-07 01:24:14 +0300

    Update Örnek02.java

--- Örnek02.java
+++ Örnek02.java
@@ -2,7 +2,7 @@ package ornekler;
 import java.util.Scanner;
 public class Örnek02 {
 	public static void main(String[] args) {
-		//Klavyeden "Dur" yazılmadığı sürece girilen sayıları toplayan program (WORK IN PROGRESS)
+		//Klavyeden "Dur" yazılmadığı sürece girilen sayıları toplayan program
 		Scanner tara = new Scanner(System.in);
 		int i=0, top=0;
 		String dur;
