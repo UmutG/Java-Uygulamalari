044d2205cf6ad31925537aa8bc794d11ca1dcae4
Umut Geyik <umut.geyik@hotmail.com>
2018-01-07 16:06:21 +0300

    Add files via upload

---
+++ Örnek42.java
@@ -0,0 +1,20 @@
+package ornekler;
+import java.util.Random;
+public class Örnek42 {
+	public static void main(String[] args) {
+		//Rastgele üretilen 10 sayının en büyüğünü ve en küçüğünü bulan program
+		int ek=Integer.MAX_VALUE, eb=0;
+		Random rast = new Random();
+		for (int i=1; i<=10; i++)
+		{
+			int a = rast.nextInt(999);
+			System.out.println(i + ". sayı: " + a);
+			if(a>=eb)
+				eb=a;
+			if(a<=ek)
+				ek=a;
+		}
+			System.out.println(ek + " en küçük sayıdır.");
+			System.out.print(eb + " en büyük sayıdır.");
+	}
+}
