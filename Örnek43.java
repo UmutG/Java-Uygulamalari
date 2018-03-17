22410096800d187c8da62b0dcd43d9cc8cda6d32
Umut Geyik <umut.geyik@hotmail.com>
2018-03-17 15:54:14 +0300

    Update Örnek43.java

--- Örnek43.java
+++ Örnek43.java
@@ -2,6 +2,7 @@ package ornekler2;
 import javax.swing.JFrame;
 public class GuessGame {
     public static void main(String args[]) throws Exception {
+        //Sayı tahmin oyunu, kullanıcının 10 hakkı var. Sayıyla olan uzaklığını belirtiyor.
     	Örnek43Kütüphane SayiTahmin = new Örnek43Kütüphane();
         SayiTahmin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         SayiTahmin.setSize(500, 150);
